1743e05b5783413fa69d6046a5926cbda3ec82f2
Alexey Sitka <alexey.sitka@gmail.com>
2022-10-13 18:01:44 +0200

    Refactored namespacing.

--- src/QueryBuild/EnumQueryBuilder.php
+++ src/QueryBuild/EnumQueryBuilder.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Enumeum\DoctrineEnum;
+namespace Enumeum\DoctrineEnum\QueryBuild;
 
 use Enumeum\DoctrineEnum\Definition\DatabaseDefinition;
 use Enumeum\DoctrineEnum\Definition\Definition;
@@ -19,9 +19,14 @@ use function sprintf;
 
 class EnumQueryBuilder
 {
+    private const TEMPORARY_SUFFIX = '__';
+
     private const TYPE_CREATE_QUERY = "CREATE TYPE %1\$s AS ENUM ('%2\$s')";
     private const TYPE_ALTER_QUERY = "ALTER TYPE %1\$s ADD VALUE IF NOT EXISTS '%2\$s'";
+    private const TYPE_ALTER_RENAME_QUERY = 'ALTER TYPE %1$s RENAME TO %2$s';
+    private const TYPE_ALTER_TABLE_QUERY = 'ALTER TABLE %2$s ALTER COLUMN %3$s TYPE %1$s USING %3$s::text::%1$s';
     private const TYPE_DROP_QUERY = 'DROP TYPE %1$s';
+
 
     public static function buildEnumTypeCreateSql(Definition $definition): iterable
     {
@@ -46,6 +51,21 @@ class EnumQueryBuilder
     public static function buildEnumTypeDropSql(Definition $definition): iterable
     {
         return [sprintf(self::TYPE_DROP_QUERY, $definition->name)];
+    }
+
+    public static function buildEnumTypeRenameToTemporarySql(Definition $definition): iterable
+    {
+        return [sprintf(self::TYPE_ALTER_RENAME_QUERY, $definition->name, $definition->name . self::TEMPORARY_SUFFIX)];
+    }
+
+    public static function buildEnumTypeDropTemporarySql(Definition $definition): iterable
+    {
+        return [sprintf(self::TYPE_DROP_QUERY, $definition->name . self::TEMPORARY_SUFFIX)];
+    }
+
+    public static function buildEnumTypeAlterColumnSql(Definition $definition, string $table, string $column): iterable
+    {
+        return [sprintf(self::TYPE_ALTER_TABLE_QUERY, $definition->name, $table, $column)];
     }
 
     public static function buildEnumTypeDropSqlByDatabaseDefinition(DatabaseDefinition $definition): iterable

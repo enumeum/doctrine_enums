e83fe189a114cc7f2ae70cffd1f83a1a60421808
Alexey Sitka <alexey.sitka@gmail.com>
2023-03-08 17:39:06 +0200

    Added backward compatibility test for existing enum fields not involved into Enumeum yet.

---
+++ tests/Enumeum/DoctrineCompatibility/BackwardCompatibilityTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Doctrine extension to manage enumerations in PostgreSQL" package.
+ * (c) Alexey Sitka <alexey.sitka@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EnumeumTests\DoctrineCompatibility;
+
+use Enumeum\DoctrineEnum\Schema\Comparator;
+use Enumeum\DoctrineEnum\Schema\SchemaManager;
+use EnumeumTests\Fixture\Entity\Entity;
+use EnumeumTests\Setup\BaseTestCaseSchema;
+
+final class BackwardCompatibilityTest extends BaseTestCaseSchema
+{
+    public function testSkipEnumUsedInEntityButNotMarkedAsEnumeumType(): void
+    {
+        $this->applySQL([
+            'CREATE TABLE entity (id INT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id))',
+            "COMMENT ON COLUMN entity.status IS 'SOME Comment'",
+        ]);
+
+        $this->composeSchema([
+            Entity::class,
+        ]);
+
+        $this->registerTypes([
+        ]);
+
+        $manager = new SchemaManager(
+            $this->getDefinitionRegistry(),
+            $this->getDatabaseDefinitionRegistry($this->em->getConnection()),
+            $this->getTableUsageRegistry($this->em->getConnection()),
+        );
+
+        $comparator = Comparator::create();
+        $diff = $comparator->compareSchemas($manager->createSchemaFromDatabase(), $manager->createSchemaFromConfig());
+
+        $updateSql = $diff->toSql();
+
+        self::assertCount(0, $updateSql);
+        self::assertSame(
+            [],
+            $updateSql,
+        );
+
+        $this->applySQLWithinTransaction($updateSql);
+    }
+
+    protected function getBaseSQL(): array
+    {
+        return [];
+    }
+}
